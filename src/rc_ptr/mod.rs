26d352ae791804e877577fe71700d7548e38e0f7
zkud <kudirko.anton@gmail.com>
2021-07-22 10:11:34 +0300

    Provided a trick with cloning for rc

--- src/rc_ptr/mod.rs
+++ src/rc_ptr/mod.rs
@@ -3,6 +3,7 @@
 use super::alloc_error;
 use crate::util::alloc as util_alloc;
 use std::rc::Rc;
+use std::mem;
 
 /// Allocates a [`Rc<T>`]
 ///
@@ -20,6 +21,6 @@ use std::rc::Rc;
 /// If allocation is not possible due to issues with memory layouts or not enough memory,
 /// it will return an [AllocError](crate::alloc_error::AllocError)
 pub fn alloc<T: Sized>() -> Result<Rc<T>, alloc_error::AllocError> {
-    let value_ptr = util_alloc::alloc_value()?;
-    Ok(unsafe { Rc::from_raw(value_ptr as *const T) })
+    let rc_ptr: *mut Rc<T> = util_alloc::alloc_zeroed_value()?;
+    Ok((unsafe { &*rc_ptr }).clone())
 }
